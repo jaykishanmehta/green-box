3e81c3b6fe66f3945c35e88dbac596773c005bfc
Jay Mehta <jaykishanmehta1991@gmail.com>
2023-03-25 14:42:32 +0530

    Initial commit

---
+++ src/test/java/com/hackathon/brainbox/greenbox/GreenBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.brainbox.greenbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
